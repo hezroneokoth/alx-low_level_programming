2d1400ec1f6f5051e1724e277ab58341159aa58d
hezroneokoth <hezrone.okoth@icloud.com>
2023-02-19 18:35:18 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+int main()
+{
+       	// print lowercase alphabet
+	for (char c = 'a'; c <= 'z'; c++) {
+        putchar(c);
+    }
+     // print uppercase alphabet
+      for (char c = 'A'; c <= 'Z'; c++) {
+        putchar(c);
+    }
+    // print new line
+    putchar('\n');
+    return 0;
+}
