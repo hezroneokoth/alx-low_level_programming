ae40171e9076bd57120ecba5b4941f0a98224505
hezroneokoth <hezrone.okoth@icloud.com>
2023-02-23 15:08:00 -0800

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,23 +9,27 @@
 
 void print_triangle(int size)
 {
-	int i, j;
-
 	if (size <= 0)
 	{
-		putchar("\n");
-	}
-
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j < size; j++)
-		{
-			_putchar(' ');
-		}
-		for (j = 1; j <= i; j++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
 	}
 }
