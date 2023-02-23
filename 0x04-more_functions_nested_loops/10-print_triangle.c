39214da87dca855cfd99dae4dd5cab8d9c04dd75
hezroneokoth <hezrone.okoth@icloud.com>
2023-02-23 13:41:55 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ * @size: size of the triangle
+ * 
+ * Return: print new line if size is 0 or less
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
