08b221fb77da8c49e642c1171e6c7b13d2ec8e09
hezroneokoth <hezrone.okoth@icloud.com>
2023-02-23 00:16:13 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,8 +2,9 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many dates are left in the year, taking leap years into account
- * @month: month in the number format
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year taking leap years into account
+ * @month: month in number format
  * @day: day of the month
  * @year: year
  * Return: void
