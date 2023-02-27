7065e5accb2c5cdff6e84f2d8273efa79c67189e
hezroneokoth <hezrone.okoth@icloud.com>
2023-02-27 13:19:11 -0800

    Half the lies they tell about me are not true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ * starting with the first character
+ * followed by a new line.
+ * @str: input
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i += 2;
+	}
+	putchar('\n');
+}
