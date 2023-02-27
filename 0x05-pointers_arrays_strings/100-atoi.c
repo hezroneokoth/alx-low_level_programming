6fd83c1d79068ab871eed366c5d4998c748ccfa1
hezroneokoth <hezrone.okoth@icloud.com>
2023-02-27 15:29:04 -0800

    Other adjustements on 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,25 +12,26 @@ int _atoi(char *s)
 {
 	int num = 0;
 	int sign = 1;
-	int i = 0;
+	int digit;
 
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] == '-')
+		if (*s == '-')
 		{
-			sign = -1;
+			sign *= -1;
 		}
-		else if (s[i] >= '0' && s[i] <= '9')
+		else if (*s >= '0' && *s <= '9')
 		{
-			result = result * 10 + (s[i] - '0');
+			digit = *s - '0';
+			num = num * 10 + digit;
 		}
-		else if (result > 0)
+		else if (num > 0)
 		{
 			break;
 		}
 
-		i++;
+		s++;
 	}
 
-	return (sign * result);
+	return (num * sign);
 }
