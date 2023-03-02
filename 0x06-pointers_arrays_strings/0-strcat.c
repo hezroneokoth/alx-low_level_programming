59ade7b91b4e4232df4730cd38a15598f8c1e0c5
hezroneokoth <hezrone.okoth@icloud.com>
2023-03-02 02:50:26 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,11 +8,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = strlen(dest);
-	int i;
+	int dest_len = 0;
+	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i]
-			dest[dest_len + i] = '\0';
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
 	return (dest);
 }
