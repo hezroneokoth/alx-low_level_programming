49bc36e7ebba1b3171cd0197022b953d764f69ce
hezroneokoth <hezrone.okoth@icloud.com>
2023-03-02 03:45:19 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - function changes lowercase letters of string to uppercase
+ * @s: the string
+ * Return: Always 0
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+	}
+	return (s);
+}
