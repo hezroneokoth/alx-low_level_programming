3d2b72fdc848611d76981c164c91c19ab44c34d1
hezroneokoth <hezrone.okoth@icloud.com>
2023-03-10 11:16:33 -0800

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,16 @@
 #include "main.h"
 
-void _puts(char *s)
-{
+/**
+ * _puts -  function that prints a string, followed by a new line, to stdout
+ * @str: the character
+ */
 
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(str);
+		str++;
+	}
+	putchar('\n');
 }
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,6 +1,34 @@
 #include "main.h"
-
+#include <stddef.h>
+/**
+ * _strstr - Locates a substring
+ * @haystack: Pointer to string to search
+ * @needle: Pointer to substring to locate
+ *
+ * Return: Pointer to the beginning of the located substring
+ * or NULL if the substring is not found.
+ */
 char *_strstr(char *haystack, char *needle)
 {
+	char *start, *search;
 
+	while (*haystack != '\0')
+	{
+		start = haystack;
+		search = needle;
+
+		while (*search == *haystack && *search != '\0'
+		       && *haystack != '\0')
+		{
+			search++;
+			haystack++;
+		}
+
+		if (*search == '\0')
+			return (start);
+
+		haystack = start + 1;
+	}
+
+	return (NULL);
 }
