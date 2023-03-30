886a8de83832910851500d933b368d058126cd1c
hezroneokoth <hezrone.okoth@icloud.com>
2023-03-30 13:23:24 -0700

    Writing a 64bit program in assembly printing Hello Holberton followed by a new line

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+global main
+extern printf
+
+section .data
+hello db 'Hello, Holberton', 0
+newline db 10, 0
+
+section .text
+main:
+mov rdi, hello
+mov rsi, 0
+call printf
+mov rdi, newline
+mov rsi, 0
+call printf
+
+xor eax, eax
+ret
