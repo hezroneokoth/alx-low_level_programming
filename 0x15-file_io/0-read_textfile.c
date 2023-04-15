e6c9e8fe41c6c39696a6a6d2e8b8b97eaa16c2cc
hezroneokoth <hezrone.okoth@icloud.com>
2023-04-15 15:48:02 -0700

    A Function that reads a textfile and prints it to standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,21 +13,18 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
-	char buffer[1024];
+	ssize_t w, r;
+	char *buf;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-		return (0);
+	buf = malloc(sizeof(char) * letters);
+	r = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-		return (0);
-
+	free(buf);
 	close(fd);
-	return (bytes_read);
+	return (w);
 }
