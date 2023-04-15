ba2e823de31f02c6fee6f9186b658d962e5952da
hezroneokoth <hezrone.okoth@icloud.com>
2023-04-15 16:20:37 -0700

    Updated function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,15 +14,19 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	bytes_written = write(fd, text_content, len);
-
-	if (fd == -1 || bytes_written == -1)
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
+		while (text_content[len])
 			len++;
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
