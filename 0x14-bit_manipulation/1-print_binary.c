7ad766373792a15ad6544e1411aa12438837aab0
hezroneokoth <hezrone.okoth@icloud.com>
2023-05-26 12:40:30 -0700

    Print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_binary - function prints the binary rep of a number
- * @n: the number to print in binary form
+ * @n: the no to print in binary form
  */
 void print_binary(unsigned long int n)
 {
 	int bitcount = 0;
 	unsigned long int mask = 1;
 
-	while ((mask << 1) <= n)
+	while (mask <= n)
 		mask <<= 1;
 	while (mask != 0)
 	{
@@ -16,6 +16,6 @@ void print_binary(unsigned long int n)
 		mask >>= 1;
 		bitcount++;
 	}
-	if (bitcount == 0)
+	if (n == 0)
 		_putchar('0');
 }
